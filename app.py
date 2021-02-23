ce289f854811fd0d7fd2157a00064354c3af7f64
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-02-23 15:54:51 +0100

    PEP 8 changements

--- app.py
+++ app.py
@@ -1,51 +1,57 @@
 import json
 import logging
 from flask import Flask, abort
-import pandas
-import pandas as pd
-from test_panda import choix_pays,convert_list
+from test_panda import lastest_by_country, convert_country_to_liste
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-convert_list
-
-
+convert_country_to_liste
 
 """
+
 Récupérer les infos du fichier :
 https://data.un.org/_Docs/SYB/CSV/SYB63_310_202009_Carbon%20Dioxide%20Emission%20Estimates.csv
+
 """
+
 
 @app.route('/')
 def hello_world():
-    #utilisé pour tester si l'app fonctionne bien
+    # utilisé pour tester si l'app fonctionne bien
     return "hello world"
+
 
 @app.route('/latest_by_country/<country>')
 def by_country(country):
-    #on veut la valeur la plus récente des emissions totales pour le pays demandé
+    # on veut la valeur la plus récente des emissions totales
+    # pour le pays demandé
     # logging.debug(f"Pays demandé : {country}")
-    return choix_pays(country)
+    return lastest_by_country(country)
+
 
 @app.route('/average_by_year/<year>')
 def average_for_year(year):
-    #on cherche la moyenne des émissions totales au niveau mondial pour une année demandée
+    # on cherche la moyenne des émissions totales au niveau mondial
+    # pour une année demandée
     logging.debug(f"Année demandée : {year}")
-    if year=="1975":
-        return json.dumps({"year":"1975", "total":12333555.9})
+    if year == "1975":
+        return json.dumps({"year": "1975", "total": 12333555.9})
     else:
         abort(404)
+
 
 @app.route('/per_capita')
 def per_capita(country):
     logging.debug(f"Pays demandé : {country}")
-    
-    if country.lower()=="albania":
-        return json.dumps({1975:4338.334, 1985:6929.926, 1995:1848.549, 2005:3825.184, 2015:3824.801, 2016:3674.183, 2017:4342.011})
+    if country.lower() == "albania":
+        return json.dumps({1975: 4338.334, 1985: 6929.926, 1995: 1848.549,
+                           2005: 3825.184, 2015: 3824.801, 2016: 3674.183,
+                           2017: 4342.011})
     else:
-        #erreur 404 si on demande un pays qui n'est pas connu
+        # erreur 404 si on demande un pays qui n'est pas connu
         abort(404)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
--- test_panda.py
+++ test_panda.py
@@ -1,22 +1,22 @@
 import json
 import pandas
-import pandas as pd
-url = pandas.read_csv('https://data.un.org/_Docs/SYB/CSV/SYB63_310_202009_Carbon%20Dioxide%20Emission%20Estimates.csv', header = 2, names = ['id', 'Country', 'year','emission','values','footnote','source'])
-#url = url.drop(url.columns[0], axis=1)
 
-def convert_list():
-    area_dict = set(url['Country'].tolist())
+url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
+                      'emission', 'values', 'footnote', 'source'])
+# url = url.drop(url.columns[0], axis=1)
 
-def choix_pays(convert_list):
-    url = pandas.read_csv('co2.csv', header = 2, names = ['id', 'Country', 'year','emission','values','footnote','source'])
-    data_pays = url.loc[url['Country'].isin([convert_list])].sort_values(['year'], ascending = False)
+
+def convert_country_to_liste():
+    dict_country = set(url['Country'].tolist())
+
+
+def lastest_by_country(convert_country_to_liste):
+    url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
+                          'emission', 'values', 'footnote', 'source'])
+    data_pays = url.loc[url['Country'].isin([convert_country_to_liste])].sort_values(['year'], ascending=False)
     # data_pays = (csv.sort_values(['year'], ascending=False)[result])
     res = {}
     res["Country"] = str(data_pays.iloc[0][1])
     res["year"] = int(data_pays.iloc[0][2])
     res["values"] = float(data_pays.iloc[0][4])
     return json.dumps(res)
-
-
-
-
