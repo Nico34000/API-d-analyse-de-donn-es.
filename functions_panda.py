b2760f822fe4885fb4d4cb478859160c812128fa
Nico34000 <prodhomme.nicolas@hotmail.fr>
2021-02-24 16:17:11 +0100

    fonctions ok , pep 8 ok, ajout fichier csv

--- functions_panda.py
+++ functions_panda.py
@@ -1,53 +1,55 @@
 import json
 import pandas
 
-url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
-                      'emission', 'values', 'footnote', 'source'])
-# url = url.drop(url.columns[0], axis=1)
 
-
-def convert_country_to_liste():
-    dict_country = set(url['Country'].tolist())
-
-
-def lastest_by_country(convert_country_to_liste):
-    url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
+def csv_file():
+    csv = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
                           'emission', 'values', 'footnote', 'source'])
-    data_pays = url.loc[url['Country'].isin([convert_country_to_liste])].sort_values(['year'], ascending=False)
-    # data_pays = (csv.sort_values(['year'], ascending=False)[result])
-    res = {}
-    res["Country"] = str(data_pays.iloc[0][1])
-    res["year"] = int(data_pays.iloc[0][2])
-    res["values"] = float(data_pays.iloc[0][4])
-    return json.dumps(res)
+    return csv
+
+
+def country_list():
+    df = csv_file()
+    country_list = set(df['Country'].tolist())
+    return country_list
+
+
+def latest_by_country(country):
+    df = csv_file()
+    df = df.loc[df['Country'].isin([country])].sort_values(['year'],
+                                                           ascending=False)
+    result = {}
+    result["country"] = str(df.iloc[0][1])
+    result["year"] = int(df.iloc[0][2])
+    result["emissions"] = float(df.iloc[0][4])
+    return json.dumps(result)
+
+
+def year_list():
+    df = csv_file()
+    list_year = set(df['year'].tolist())
+    return list_year
 
 
 def average_year(year):
-    url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
-                          'emission', 'values', 'footnote', 'source'])
-    data_year = url.loc[url['year'].isin([year])]
-    data_year = data_year[(data_year["emission"] == 'Emissions (thousand metric \
-    tons of carbon dioxide)')]
-    print(data_year)
-    mean_year = data_year.mean()['values']
+    df = csv_file()
+    df = df.loc[df['year'].isin([year])]
+    df = df[(df["emission"] == 'Emissions \
+(thousand metric tons of carbon dioxide)')]
+    mean_year = df.mean()['values']
     result = {}
     result["year"] = year
     result["total"] = float(mean_year)
     return json.dumps(result)
-  
-  
+
+
 def per_capi(country):
-    url = pandas.read_csv('co2.csv', header=2, names=['id', 'Country', 'year',
-                          'emission', 'values', 'footnote', 'source'])
-    capita = url.loc[url['Country'].isin([country])]
-    capita = capita[(capita["emission"] == 'Emissions per capita(metric tons \
-    of carbon dioxide)')]
+    df = csv_file()
+    df = df.loc[df['Country'].isin([country])]
+    df = df[(df["emission"] == 'Emissions \
+per capita (metric tons of carbon dioxide)')]
     result = {}
-    longeur = len(capita)
+    longeur = len(df)
     for i in range(longeur):
-        result[str(capita.iloc[i][2])]=float(capita.iloc[i][4])
+        result[int(df.iloc[i][2])] = float(df.iloc[i][4])
     return json.dumps(result)
-
-
-
-
