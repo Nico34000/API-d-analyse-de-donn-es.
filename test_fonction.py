f339ce8f8971c95fc336f90bb1d3db9c8e4bea44
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-02-25 16:21:26 +0100

    Fonction unittest Average for year

--- test_fonction.py
+++ test_fonction.py
@@ -12,5 +12,15 @@ class TestMethods(unittest.TestCase):
         self.assertIsNot({1999}, latest_by_country('France'))
         self.assertIsInstance(latest_by_country('Belgium'), dict)
 
+       
+    def test_average_for_year (self):
+        """Test la fonction average_year."""
+        self.assertEqual({"total": 217093.22722535214, "year": 2016}, average_year(2016))
+        self.assertIsNotNone(1985 ,average_year(1985))
+        self.assertIsNotNone({"year": 1975} ,average_year(1975))
+        self.assertIsInstance(average_year(1995), dict)
+        self.assertIsNot({2000}, average_year(1995))
+        
+        
 if __name__ == '__main__':
     unittest.main()
