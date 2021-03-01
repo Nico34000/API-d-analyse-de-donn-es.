5f3b1600d8b54adb4b09d37a56787585ef15fd6f
Nico34000 <77361662+Nico34000@users.noreply.github.com>
2021-03-01 14:02:29 +0100

    Update api.py

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ def by_country(country):
     """
     This function is used when the app.route is released.
     If the selected contry by the user is in the liste 
-    we call  the function latest_by_country.
+    we call  the function latest_by_country
     If the country isn't in our liste with logger.error
     we will print a message of error saying that the 
     selected country doesn't exist.
